bb530688cd3daac38fffe85f74c0793353d59b6c
JOSEL-PARADA <a00939669@tec.mx>
2025-10-31 09:18:42 -0600

    Add files via upload

---
+++ Proyecto_south_german_credit_g57/notebooks/config.py
@@ -0,0 +1,10 @@
+
+
+# Variables numéricas:
+num_col = ['duration', 'amount', 'age']
+
+# Variables ordinales:
+ord_col = ['employment_duration', 'installment_rate', 'present_residence', 'property', 'number_credits', 'job']
+
+# Variables nominales & binarias:
+cat_col = ['status', 'credit_history', 'purpose',  'savings', 'people_liable', 'personal_status_sex', 'other_debtors', 'other_installment_plans', 'housing', 'telephone', 'foreign_worker']
