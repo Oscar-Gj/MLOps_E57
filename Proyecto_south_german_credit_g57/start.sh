17965ffa252b570a58b96add1d894090a84953cc
JulianMorenoOvando <a01795915@tec.mx>
2025-11-13 11:26:20 -0600

    App: PATH Fixed

--- Proyecto_south_german_credit_g57/start.sh
+++ Proyecto_south_german_credit_g57/start.sh
@@ -11,8 +11,8 @@ trap cleanup SIGINT SIGTERM EXIT
 
 # 1. Lanza la API de FastAPI en segundo plano
 echo "Iniciando FastAPI (Uvicorn) en el puerto 8000..."
-uvicorn app.main:app --host 127.0.0.1 --port 8000 &
+uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
 # 2. Lanza la App de Streamlit en primer plano
 echo "Iniciando Streamlit en el puerto 8001..."
-streamlit run app/streamlit_app.py --server.port 8001 --server.address 127.0.0.1
+streamlit run app/streamlit_app.py --server.port 8001 --server.address 0.0.0.0
